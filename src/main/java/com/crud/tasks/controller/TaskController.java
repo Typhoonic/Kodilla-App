34fcfaaeeea78fa926dc98c069ee07f89d1e95e8
Kamil Dawid <koxxor12@gmail.com>
2021-07-10 00:18:02 +0200

    [19.2] - throw NoSuchElementException

--- src/main/java/com/crud/tasks/controller/TaskController.java
+++ src/main/java/com/crud/tasks/controller/TaskController.java
@@ -4,14 +4,14 @@ import com.crud.tasks.domain.Task;
 import com.crud.tasks.domain.TaskDto;
 import com.crud.tasks.mapper.TaskMapper;
 import com.crud.tasks.service.DbService;
+import javassist.NotFoundException;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.hibernate.annotations.NotFound;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.*;
-import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/v1/task")
@@ -30,9 +30,17 @@ public class TaskController {
     }
 
     @RequestMapping(method = RequestMethod.GET, value = "getTask")
-    public TaskDto getTask(Long taskId){
+    public TaskDto getTask(Long taskId) {
         Optional<Task> taskListById = dbService.getTaskById(1L);
-        return taskMapper.mapToTaskDto(taskListById.get());
+        TaskDto task = null;
+
+        try{
+            task = taskMapper.mapToTaskDto(taskListById.get());
+        }catch(NoSuchElementException e){
+            e.getStackTrace();
+        }
+
+        return task;
     }
 
     @RequestMapping(method = RequestMethod.DELETE, value = "deleteTask")
