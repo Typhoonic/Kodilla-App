b47aacafdd96c1cec42406e320f6c2eec6643c19
Typhoonic <81630895+Typhoonic@users.noreply.github.com>
2021-09-22 20:14:49 +0200

    Add exercises

--- src/main/java/com/crud/tasks/controller/TrelloController.java
+++ src/main/java/com/crud/tasks/controller/TrelloController.java
@@ -3,23 +3,24 @@ package com.crud.tasks.controller;
 import com.crud.tasks.domain.CreatedTrelloCard;
 import com.crud.tasks.domain.TrelloBoardDto;
 import com.crud.tasks.domain.TrelloCardDto;
+import com.crud.tasks.service.TrelloService;
 import com.crud.tasks.trello.client.TrelloClient;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.Optional;
 
 @RestController
-@RequestMapping("/v1/trello")
+@RequestMapping("/v1/trello/")
 @RequiredArgsConstructor
+@CrossOrigin(origins = "*")
 public class TrelloController {
 
-    private final TrelloClient trelloClient;
+    private final TrelloService trelloService;
 
     @GetMapping("getTrelloBoards")
     public List<TrelloBoardDto> getTrelloBoards(){
-        return trelloClient.getTrelloBoards();
+        return trelloService.fetchTrelloBoards();
 
        /* Optional.ofNullable(trelloBoards).get().stream()
                 .filter(n -> n.getName()!=null)
@@ -41,7 +42,7 @@ public class TrelloController {
 
     @PostMapping("createTrelloCard")
     public CreatedTrelloCard createTrelloCard(@RequestBody TrelloCardDto trelloCardDto){
-        return trelloClient.createNewTask(trelloCardDto);
+        return trelloService.createdTrelloCard(trelloCardDto);
     }
 
 }
--- src/main/java/com/crud/tasks/domain/Mail.java
+++ src/main/java/com/crud/tasks/domain/Mail.java
@@ -1,9 +1,11 @@
 package com.crud.tasks.domain;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 
 @Getter
+@AllArgsConstructor
 @Builder
 public class Mail {
     private final String mailTo;
