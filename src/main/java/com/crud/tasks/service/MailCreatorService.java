aff48fc341caca8de220390386a3fb24aa2047db
Typhoonic <81630895+Typhoonic@users.noreply.github.com>
2021-10-22 21:20:44 +0200

    [32.2] - fill the rest of email

--- src/main/java/com/crud/tasks/service/MailCreatorService.java
+++ src/main/java/com/crud/tasks/service/MailCreatorService.java
@@ -1,6 +1,7 @@
 package com.crud.tasks.service;
 
 import com.crud.tasks.config.AdminConfig;
+import com.crud.tasks.config.CompanyConfig;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
@@ -9,6 +10,10 @@ import org.thymeleaf.context.Context;
 
 @Service
 public class MailCreatorService {
+
+    @Autowired
+    CompanyConfig companyConfig;
+
     @Autowired
     AdminConfig adminConfig;
 
@@ -22,6 +27,12 @@ public class MailCreatorService {
         context.setVariable("tasks_url", "http://localhost:8888/crud/");
         context.setVariable("button", "Visit website");
         context.setVariable("admin_name", adminConfig.getAdminName());
+        context.setVariable("preview", "Information about create task in trello card");
+        context.setVariable("goodbye", "Have a nice day!");
+        context.setVariable("company_name", companyConfig.getName());
+        context.setVariable("company_goal", companyConfig.getGoal());
+        context.setVariable("company_email", companyConfig.getEmail());
+        context.setVariable("company_phone", companyConfig.getPhone());
         return templateEngine.process("mail/created-trello-card-mail", context);
     }
 }
